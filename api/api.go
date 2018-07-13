f8223a5d8582d388217d8658405529749cdbe671
del680202 <del680202@gmail.com>
2018-07-13 15:24:20 +0900

    Fixed dependency problem

--- api/api.go
+++ api/api.go
@@ -1,7 +1,7 @@
 package goofys
 
 import (
-	. "github.com/kahing/goofys/internal"
+	. "github.com/del680202/goofys/internal"
 
 	"context"
 	"fmt"
--- main.go
+++ main.go
@@ -16,8 +16,8 @@
 package main
 
 import (
-	goofys "github.com/kahing/goofys/api"
-	. "github.com/kahing/goofys/internal"
+	goofys "github.com/del680202/goofys/api"
+	. "github.com/del680202/goofys/internal"
 
 	"fmt"
 	"os"
